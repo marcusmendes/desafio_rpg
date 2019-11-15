076d48e27cf40283c40639675a1466573072c5f8
Marcus Maciel <mendesxx.ti@hotmail.com>
2019-11-15 18:34:16 -0300

    Ajustes na configuração do docker

--- web/src/services/api.js
+++ web/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+console.tron.debug(process);
+
 const api = axios.create({
-  baseURL: 'http://localhost:8001/',
+  baseURL: 'http://localhost:8000/',
 });
 
 export default api;
